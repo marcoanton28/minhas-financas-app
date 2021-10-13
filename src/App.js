447d2ab4ac3147e6d1ae9c70f0aaa06db6322344
marcoanton28 <marquinhos.8sos8@gmail.com>
2021-10-13 16:40:27 -0300

    Iniciando a aplicação do front-end

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Login from "./views/login";
+
+import "bootswatch/dist/darkly/bootstrap.css";
+import "./custom.css";
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <Login></Login>
+      </div>
+    );
+  }
+}
+
+export default App;
